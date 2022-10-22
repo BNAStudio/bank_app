edae37a4b7d91c9e89ac8560ac11650dd99f96a4
Daniel Marcovich <danielmark999@gmail.com>
2022-10-22 18:00:36 -0500

    fix(components): fix identation
    fix NavBar component identation

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -12,12 +12,13 @@ const NavBar = () => {
 				{navLinks.map((nav, index) => (
 					<li
 						key={nav.id}
-						className={`font-poppins font-normal cursor-pointer text-[16px] 
+						className={`
+                            font-poppins 
+                            font-normal 
+                            cursor-pointer 
+                            text-[16px] 
                             text-white 
-                            ${
-															index === navLinks.length - 1 ? "mr-0" : "mr-10"
-														}`}
-					>
+                            ${index === navLinks.length - 1 ? "mr-0" : "mr-10"}`}>
 						<a href={`#${nav.id}`}> {nav.title} </a>
 					</li>
 				))}
@@ -32,15 +33,30 @@ const NavBar = () => {
 				/>
 
 				<div
-					className={`${
-						toggle ? "flex" : "hidden"
-					} p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
+					className={`
+                        ${toggle ? "flex" : "hidden"} 
+                        p-6 
+                        bg-black-gradient 
+                        absolute 
+                        top-20 
+                        right-0 
+                        mx-4 
+                        my-2 
+                        min-w-[140px] 
+                        rounded-xl 
+                        sidebar`}
 				>
 					<ul className="list-none flex flex-col justify-end items-center flex-1">
 						{navLinks.map((nav, index) => (
 							<li
 								key={nav.id}
-								className={`font-poppins font-normal cursor-pointer text-[16px] text-white ${index === navLinks.length - 1 ? "mr-0" : "mb-4"}`}>
+								className={`
+                                    font-poppins 
+                                    font-normal 
+                                    cursor-pointer 
+                                    text-[16px] 
+                                    text-white 
+                                    ${index === navLinks.length - 1 ? "mr-0" : "mb-4"}`}>
 								<a href={`#${nav.id}`}> {nav.title} </a>
 							</li>
 						))}
