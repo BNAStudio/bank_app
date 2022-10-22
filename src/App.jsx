cdfade2601bb839c7bf8991f86ef1d313f1efc9f
Daniel Marcovich <danielmark999@gmail.com>
2022-10-22 16:09:41 -0500

    feat(core): initial config
    adding constants | adding styles |  adding tailwind.config.cjs confi | adding tailwind library |
    adding postcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
-const App = () => {
-	return (
-		<div>
-			<h1>REACT TEMPLATE</h1>
-		</div>
-	);
-};
+
+const App = () => (
+	<div className="bg-primary w-full overflow-hidden">
+		<h1>REACT TEMPLATE</h1>
+	</div>
+);
 
 export default App;
