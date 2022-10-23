7cbb6e4b3c6c6620beda29bac048c241028fe111
Daniel Marcovich <danielmark999@gmail.com>
2022-10-23 02:27:03 -0500

    feat(core | components): create components | adding esLint rule
    finish Hero component | create Stats, Hero, Button Business, Billing | adding styles for all
    components

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,3 +1,9 @@
-const Button = () => <div>Button</div>;
+/* eslint-disable react/prop-types */
 
-export default Button;
+const Button = ({ styles }) => (
+    <button type="button" className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles}`}>
+        Get started
+    </button>
+)
+
+export default Button
